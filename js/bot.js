1bfaf1cd50fe63cd311cd17a16cacd93a969b511
qqloses <packetloses@gmail.com>
2024-10-21 21:06:09 +0300

    Update bot.js

--- js/bot.js
+++ js/bot.js
@@ -5,23 +5,27 @@ const config = require('../json/config.json');
 async function connectBot() {
   const client = new Client({ checkUpdate: false });
 
-  const channelId = config.channelid;
-  const guildId = config.guildid;
-  const selfDeaf = config.selfdeaf;
-  const selfMute = config.selfmute;
+  const { channelid: channelId, guildid: guildId, selfdeaf: selfDeaf, selfmute: selfMute } = config;
 
   client.on('ready', async () => {
     console.log(`Bot connected. username: ${client.user.username}`);
 
     const connectToVoiceChannel = () => {
       const connection = getVoiceConnection(guildId);
-      
+
       if (connection) {
         if (connection.joinConfig.channelId !== channelId) {
-          connection.destroy(); 
+          connection.destroy();
         } else {
           return; 
         }
+      }
+
+      const voiceChannel = client.channels.cache.get(channelId);
+
+      if (voiceChannel.members.size >= voiceChannel.userLimit) {
+        console.log(`Voice channel '${voiceChannel.name}' is full.`);
+        return;
       }
 
       joinVoiceChannel({
